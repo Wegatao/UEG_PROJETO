54f0f648f4de5958277ca3a3bfefbe04ee1c8774
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-08-28 15:43:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def index():
         return f"Erro: {e}"
         
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
 
 
