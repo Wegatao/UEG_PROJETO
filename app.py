9b6c6fe57d4df40f9a61238d0fde0811dbb73052
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-08-28 18:59:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from connection.bd_connection import Conexao
+import os
 
 app = Flask(__name__)
 
@@ -18,5 +19,8 @@ def index():
    
 
 criar.fechar()
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
