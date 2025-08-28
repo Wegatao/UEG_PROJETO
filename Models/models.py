a61f193d873fdb59efb84e74b4551a2f6908e04c
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-08-28 16:11:47 -0300

    Update models.py

--- Models/models.py
+++ Models/models.py
@@ -6,11 +6,12 @@ class Cricao_tabela:
     
     def __init__(self):
         self.db = Conexao()
-        self.cursor = self.db.get_cursor()
+        cursor = self.db.get_cursor()
 
     def set_criarUsuario(self):
+        cursor = self.db.get_cursor()
         try:
-            self.cursor.execute("""
+            cursor.execute("""
                 CREATE TABLE IF NOT EXISTS usuarios (
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     nome VARCHAR(255) NOT NULL,
@@ -20,15 +21,16 @@ class Cricao_tabela:
                 )""")
             self.db.commit()
         finally:
-            self.cursor.close()
+            cursor.close()
 
     def set_inserirusuario(self, nome, email, senha, dataIncricao):
+        cursor = self.db.get_cursor()
         try:
-            self.cursor.execute("insert into usuarios(nome, email, senha, dataIncricao) Values(%s,%s,%s,%s)",
+            cursor.execute("insert into usuarios(nome, email, senha, dataIncricao) Values(%s,%s,%s,%s)",
                        (nome, email, senha, dataIncricao))
             self.db.commit()
         finally:
-            self.cursor.close()
+            cursor.close()
 
 
 
