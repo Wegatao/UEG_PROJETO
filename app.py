c43a48887d313f7d050b7d10b76e55c486bd72a9
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-08-28 16:19:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from Models.models import Cricao_tabela
-import os
+
 app = Flask(__name__)
 
 # Crie a conexão com seus dados MySQL
@@ -12,14 +12,11 @@ def index():
     try:
         criar.set_criarUsuario()
         criar.set_inserirusuario("welber", "@.com.br", "2332323", "2025-12-10")
-
+        criar.get_fechar()
         return 'usuario inserido com sucesso!'
+    
     except Exception as e:
         return f"Erro: {e}"
-        
+   
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
-
-
-
-
+    app.run(debug=True)
