44c1e586f01f0006b8feafa2218eb1a6c093c54b
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-08-29 10:57:43 -0300

    Update models.py

--- Models/models.py
+++ Models/models.py
@@ -1,40 +1,46 @@
-from connection.bd_connection import Conexao
-
-
-
-class Cricao_tabela:
-    
-    def __init__(self):
-        self.db = Conexao()
-        
-
-    def set_criarUsuario(self):
-        cursor = self.db.get_cursor()
-        try:
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS usuarios (
-                    id INT AUTO_INCREMENT PRIMARY KEY,
-                    nome VARCHAR(255) NOT NULL,
-                    email VARCHAR(255),
-                    senha VARCHAR(255) NOT NULL,
-                    dataInscricao DATE NOT NULL
-                )""")
-            self.db.commit()
-        finally:
-            cursor.close()
-
-    def set_inserirusuario(self, nome, email, senha, dataInscricao):
-        cursor = self.db.get_cursor()
-        try:
-            cursor.execute("insert into usuarios(nome, email, senha, dataInscricao) Values(%s,%s,%s,%s)",
-                       (nome, email, senha, dataInscricao))
-            self.db.commit()
-        finally:
-            cursor.close()
-
-    def get_fechar(self):
-        return self.db.fechar()
+import mysql.connector
+from mysql.connector import Error
        
+# Classe que gerencia a conexão e operações com o banco de dados
+class Conexao:
+        def __init__(self, config):
+         self.config = config # Configuração do banco de dados
+
+        def conectar(self):
+         try:
+            conexao = mysql.connector.connect(**self.config)
+            if conexao.is_connected():
+                print("Conexão bem-sucedida com o banco de dados.")
+                return conexao
+         except Error as e:
+                print(f"Erro ao conectar no MySQL: {e}")
+                return None
+    
+
+       # Cria a tabela "cooperados" se ela não existir
+        def set_inserirusuario(self, nome, email, senha, dataInscricao):
+          conexao = self.conectar()
+        
+          if conexao:
+            try:
+              cursor = conexao.cursor()
+              cursor.execute("""
+                INSERT INTO Pendencias (nome, email, senha, dataInscricao)
+                VALUES (%s, %s, %s, %s)
+                """, (nome, email, senha, dataInscricao))
+              
+              conexao.commit()
+              return {"sucesso": True, "mensagem": "Pendência cadastrada com sucesso"}
+            
+            except Error as e:
+              print(f"Erro ao cadastrar pendência: {e}")
+              return {"sucesso": False, "mensagem": f"Erro: {e}"}
+            finally:
+              conexao.close()
+            
+       
+       
+
 
 
 
