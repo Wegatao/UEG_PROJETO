bcc88bec1a49533180a9dc6b37da385ad432d8e6
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-08-29 11:14:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
 from flask import Flask
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from connection.bd_connection import Conexao
 from connection.confing import CONFING
 import os
-
-
 app = Flask(__name__)
 conexao = Conexao(CONFING)
+
+
 
 @app.route("/")
 def index():
     return jsonify('esta tudo certo')
-    
 
 @app.route("/InserirNovoUsuario", methods=["POST"])
 def cadastrarPessoa():
-    dados = ('welber','111','wewew',1010)
-    resultado = conexao.set_inserirusuario(dados)
-    return jsonify(resultado), 200 if resultado.get("sucesso") else 400
+    dados = request.json
+    resultado = conexao.set_inserirusuario(
+        dados['nome'], dados['email'], dados['senha'], dados['dataInscricao']
+    )
+    sucesso = resultado.get("mensagem") == "ok"
+    return jsonify({"sucesso": sucesso, "resultado": resultado}), 200 if sucesso else 400
+
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
+
